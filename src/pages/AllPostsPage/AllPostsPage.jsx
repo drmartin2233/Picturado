eb84f60467ae5695726641408861d375672bd2e0
David Martin <d.r.martin.wrk@gmail.com>
2023-04-11 11:35:23 -0400

    retrieve function

--- src/pages/AllPostsPage/AllPostsPage.jsx
+++ src/pages/AllPostsPage/AllPostsPage.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react';
+import { getAllPosts } from '../../utilities/posts-service';
+
 export default function AllPostsPage() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(function() {
+        console.log('print inside useEffect has fired')
+        async function retrievePosts() {
+            const retrievedPosts = await getAllPosts();
+        }
+       retrievePosts()
+    }, [posts]) 
+
     return(
             <h1>AllPostsPage</h1>
     )
---
+++ src/utilities/posts-service.js
@@ -0,0 +1,6 @@
+
+
+
+export async function getAllPosts() {
+    console.log('hello, from getAllPosts function!')
+};
