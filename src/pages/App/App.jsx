e76bb34f93503b228211c0d9b760f3c60ae306c2
David Martin <d.r.martin.wrk@gmail.com>
2023-04-11 09:40:33 -0400

    Second, minor changes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import NewOrderPage from '../NewPostPage/NewPostPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 
@@ -15,6 +15,7 @@ export default function App() {
       { user ?
           <>
             <NavBar user={user} setUser={setUser} />
+           
             <Routes>
               {/* Route components in here */}
               <Route path="/orders/new" element={<NewOrderPage />} />
